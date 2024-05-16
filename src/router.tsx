8285ac0fa3ea3bfc84b10d0fc9747045e0049b82
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2024-05-15 20:24:46 -0400

    Página: Listagem de pedidos

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { Dashboard } from "./pages/app/dashboards";
+import { Orders } from "./pages/app/orders/orders";
 import { SignIn } from "./pages/auth/sing-in";
 import { SignUp } from "./pages/auth/sing-up";
 
@@ -10,7 +11,11 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    children: [{ path: "/", element: <Dashboard /> }],
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Orders /> },
+
+    ],
   },
   {
     path: "/",
